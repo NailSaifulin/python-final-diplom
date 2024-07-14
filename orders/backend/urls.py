7faf3dd06bf950299a59efb32668803bb3812b08
nail <saifulin-nail@mail.ru>
2024-07-14 15:08:39 +0300

    register user

---
+++ orders/backend/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from backend.views import RegisterAccount
+
+app_name = 'backend'
+urlpatterns = [
+    path('user/register', RegisterAccount.as_view(), name='user-register'),
+]
--- orders/backend/views.py
+++ orders/backend/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
+from django.http import JsonResponse
+from .serializers import UserSerializer
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+
+class RegisterAccount(APIView):
+    def post(self, request, *args, **kwargs):
+
+        if {'first_name', 'last_name', 'email', 'password', 'company', 'position'}.issubset(request.data):
+
+            try:
+                validate_password(request.data['password'])
+            except Exception:
+                return JsonResponse({'Status': False, 'Error': 'Создайте пароль отвечающий требованиям'})
+
+            user_serializer = UserSerializer(data=request.data)
+            if user_serializer.is_valid():
+                user = user_serializer.save()
+                user.set_password(request.data['password'])
+                user.save()
+
+                return JsonResponse({'Status': True})
+            else:
+                return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
+
+        else:
+            return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
+
+
+class LoginAccount(APIView):
+    def post(self, request, *args, **kwargs):
+
+        if {'email', 'password'}.issubset(request.data):
+            user = authenticate(request, username=request.data['email'], password=request.data['password'])
+
+            if user is not None:
+                if user.is_active:
+                    token, _ = Token.objects.get_or_create(user=user)
+
+                    return JsonResponse({'Status': True, 'Token': token.key})
+
+            return JsonResponse({'Status': False, 'Errors': 'Не удалось авторизовать'})
+
+        return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
